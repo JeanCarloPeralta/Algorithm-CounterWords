cf17bb0596a29afe6478582f8204bfb5a7e275e0
JeanCarloPeralta <58532380+JeanCarloPeralta@users.noreply.github.com>
2021-07-01 18:10:21 -0600

    Update scritpt.js

--- scritpt.js
+++ scritpt.js
@@ -3,3 +3,10 @@ let words = 'Loren ipsun dolor sit amen.';
 let count = words.split(' ').length;
  
 alert(count);
+
+function palindromo(text){
+    return text.split('').reverse().join('') == text;
+}
+
+console.log(palindromo('reconocer'));
+console.log(palindromo('Amor a roma'));
